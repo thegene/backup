7680212109d401f0d2b8b05295546fb74da20ecb
Eugene Westbrook <eugenewestbrook@gmail.com>
2017-03-15 08:49:58 -0500

    Adds sync to prod and adds an ENV switch to it so run deliberately

--- sync/sync.sh
+++ sync/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # only run if /source and /target exist
-if [[ -d "/source" && -d "/target" ]]; then
+if [[ -d "/source" && -d "/target" && "$SYNC" = "true" ]]; then
   echo "Syncing directories"
-  rsync -av --delete /source /target
+  rsync -av --delete /source/ /target/
 else
-  echo "Not syncing: source and / or target missing"
+  echo "Not syncing: source and / or target missing or SYNC is not true"
 fi
