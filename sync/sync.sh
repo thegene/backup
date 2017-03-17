b4cc95ac46e9cbaaac2a44142f7bc29b5939a958
Eugene Westbrook <eugenewestbrook@gmail.com>
2017-03-16 21:12:38 -0500

    Add date output to sync

--- sync/sync.sh
+++ sync/sync.sh
@@ -3,6 +3,7 @@
 # only run if /source and /target exist
 if [[ -d "/source" && -d "/target" && "$SYNC" = "true" ]]; then
   echo "Syncing directories"
+  date
   rsync -av --delete /source/ /target/
 else
   echo "Not syncing: source and / or target missing or SYNC is not true"
